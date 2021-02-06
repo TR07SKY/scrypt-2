e6f91eb8b6d3a9bf6a75a9cc094acee6f84f6b85
TR07SKY <zaffranivalentino@gmail.com>
2021-02-06 06:28:06 +0000

    add working example

--- test.cpp
+++ test.cpp
@@ -6,6 +6,11 @@
 
 int main(void)
 {
-    printf("test\n");
+    const char *test = "test";
+    uint256 hash;
+
+    scryptHash(test, BEGIN(hash));
+
+    printf("%s\n", hash.GetHex().c_str());
     return 0;
 }
