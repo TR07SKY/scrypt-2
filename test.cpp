6e40e5d59dbf5fd9aac29681b4ce428abcf0a503
TR07SKY <zaffranivalentino@gmail.com>
2021-02-06 07:31:07 +0000

    add power2b hash function and update example

--- test.cpp
+++ test.cpp
@@ -1,16 +1,33 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
 #include "utils/uint256.h"
+
 #include "scrypt/scrypt.h"
+#include "power2b/yespower.h"
 
 int main(void)
 {
-    const char *test = "test";
-    uint256 hash;
+    yespower_params_t params = { /* power2b init struct */
+        .N = 1024,
+        .r = 8
+    };
 
-    scryptHash(test, BEGIN(hash));
+    uint8_t test[80] = "test";
 
-    printf("%s\n", hash.GetHex().c_str());
+    uint256 hash_scrypt;
+    yespower_binary_t hash_power2b;
+
+    scryptHash((const char *)test, BEGIN(hash_scrypt));
+    yespower_tls(test, sizeof(test), &params, &hash_power2b);
+
+    printf("Scrypt^2 hash: %s\n", hash_scrypt.GetHex().c_str());
+    printf("Power2b hash : ");
+
+    for (int i = 0; i < sizeof(hash_power2b); i++) /* print output of power2b */
+		printf("%02x", hash_power2b.uc[i]);
+
+	printf("\n");
     return 0;
 }
